8202cdfd27478e1afd1d9b147edcacd7a877070d
amirreza movahedian <amirow.movahedian@gmail.com>
2024-03-19 17:04:53 +0330

    make pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { Route,Routes,Navigate } from 'react-router-dom'
 export default function App() {
   return (
     <div className='text-sky-400'>Hello test</div>
