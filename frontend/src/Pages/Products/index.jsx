654a28f15ffbf7f687db89b2ded8485614dbcacf
amirreza movahedian <amirow.movahedian@gmail.com>
2024-04-03 17:09:16 +0330

    Filter
    make filter section responsible

--- frontend/src/Pages/Products/index.jsx
+++ frontend/src/Pages/Products/index.jsx
@@ -5,7 +5,7 @@ import fetchApi from '../../Utils/fetchApi';
 export function ProductCard({ img, name, price, desc, id, }) {
   return (
     <div class="bg-white/85 shadow-md  py-12 md:px-5 md:py-4 px-5 rounded-2xl">
-      <img className="w-full h-[50%] rounded-md" src={img} alt="products-img" />
+      <img className="w-full h-30% lg:h-[50%] rounded-md" src={img} alt="products-img" />
       <h4 className="text-center mt-3 text-2xl">{name}</h4>
       <div className="text-sky-400">PRICE : ${price}</div>
       {desc.split(" ").slice(0, 10).join(" ")}...
@@ -63,8 +63,8 @@ export default function Product() {
   console.log(products)
   return (
     <>
-      <div className='mx-4 mt-36 flex'>
-        <div className='w-[20%] border rounded-md p-3 min-h-5 select-none'>
+      <div className='mx-4 mt-16 lg:mt-36 lg:flex'>
+        <div className='w-full lg:w-[20%] border rounded-md p-3 min-h-5 select-none'>
           <label htmlFor="medium-range" className="block mb-2 text-gray-900 text-xl">Filter by Price</label>
           <p>Price : $0 - ${rangeValue}</p>
           <input id="medium-range" min={0} step={10} max={1000} type="range" value={rangeValue} onChange={(e) => setRangeValue(e.target.value)} className="w-full h-2 mb-6 bg-gray-200 rounded-lg appearance-none cursor-pointe" />
@@ -78,8 +78,8 @@ export default function Product() {
             <option value={'price:asc'}>Lowest price</option>
           </select>
         </div>
-        <div className='w-[80%]'>
-          <div className="container">
+        <div className='w-full lg:w-[80%]'>
+          <div className="lg:container mt-10 lg:mt-0">
             <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 gap-3.5 md:gap-5">
               {items}
             </div>
